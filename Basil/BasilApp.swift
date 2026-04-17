8659078461a0a568a5482dbf92d12d0ba6212c53
Shubhdeep Das <shubhdeepdas0@gmail.com>
2026-04-17 21:46:47 +0530

    Initial Commit

---
+++ Basil/BasilApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasilApp.swift
+//  Basil
+//
+//  Created by Shubhdeep Das on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BasilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
